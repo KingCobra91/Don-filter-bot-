87004995dfc976fbc838f724c5f15e9ab1bf4e6f
KingCobra91 <117145604+KingCobra91@users.noreply.github.com>
2024-01-02 09:08:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -73,8 +73,8 @@ URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', 'd472767543d3
 
      # Auto Delete For Group Message (Self Delete) #
 SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 330))
-SELF_DELETE = environ.get('SELF_DELETE', True)
-if SELF_DELETE == "True":
+SELF_DELETE = environ.get('SELF_DELETE', False)
+if SELF_DELETE == "False":
     SELF_DELETE = False
 
 # Subscribe channel Button #
